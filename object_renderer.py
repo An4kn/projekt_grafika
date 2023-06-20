7e150a1e965bd89573cfb49bb4fc1f58b3f13570
K0z1k <97193247+K0z1k@users.noreply.github.com>
2023-06-20 14:21:21 +0200

    textures finished for now

--- object_renderer.py
+++ object_renderer.py
@@ -6,8 +6,18 @@ class ObjectRenderer:
         self.game = game
         self.screen = game.screen
         self.wall_textures = self.load_wall_textures()
+        self.sky_image = self.get_texture('textures/sky.png', (WIDTH, HALF_WIDTH))
+        self.sky_offset = 0
     def draw(self):
+        self.draw_backgorund()
         self.render_game_objects()
+
+    def draw_backgorund(self):
+        self.sky_offset = (self.sky_offset + 4.5 * self.game.player.rel) % WIDTH
+        self.screen.blit(self.sky_image, (-self.sky_offset, 0))
+        self.screen.blit(self.sky_image, (-self.sky_offset + WIDTH, 0))
+
+        pg.draw.rect(self.screen, FLOOR_COLOR, (0, HALF_HEIGHT, WIDTH, HEIGHT))
 
     def render_game_objects(self):
         list_objects = self.game.raycasting.objects_to_render
